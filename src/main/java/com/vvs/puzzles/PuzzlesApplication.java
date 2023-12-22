1125acb6544692836d863649c7945b10eaf905d1
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-12-22 19:09:27 +0200

    initial commit

---
+++ src/main/java/com/vvs/puzzles/PuzzlesApplication.java
@@ -0,0 +1,12 @@
+package com.vvs.puzzles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class PuzzlesApplication implements WebMvcConfigurer {
+    public static void main(String[] args) {
+        SpringApplication.run(PuzzlesApplication.class, args);
+    }
+}
